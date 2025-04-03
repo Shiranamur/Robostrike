496eb703a8cf4667a69fa2d37210b7299c24860b
Yatarox <Faboto@yahoo.com>
2025-04-03 16:02:36 +0200

    bonne version

--- WebRobotStrike/Program.cs
+++ WebRobotStrike/Program.cs
@@ -1,28 +1,63 @@
 using BlazorApp1.Components;
+using BlazorApp1.Models;
+using Microsoft.EntityFrameworkCore;
+using BlazorApp1.UsersServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Ajout UsersSerice
+builder.Services.AddScoped<UsersService>();
+
+// bdd
+builder.Services.AddDbContext<RobostrikeContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+// IHttpContextAccessor
+builder.Services.AddHttpContextAccessor();
+
+// Ajout des services pour Razor Components
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+app.Use(async (context, next) =>
+{
+    var usersService = context.RequestServices.GetRequiredService<UsersService>();
+
+    var isAuthenticated = usersService.IsUserAuthenticated();
+
+    if (!isAuthenticated && context.Request.Path.StartsWithSegments("/"))
+    {
+        usersService.SetUserSession(147852369);
+
+
+        context.Response.Redirect("/login");
+        return;
+    }
+
+    await next.Invoke();
+});
+
+
+
+
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
 
 app.Run();
