496ad519d17daa379fa1bebbcea208cd27cdcb61
Shiranamur <ulysse.barois@gmail.com>
2025-04-09 17:04:44 +0200

    Modified queue manager to handle "error" if player is already queued.

--- tiz_teh_final_csharp_project/QueueManager.cs
+++ tiz_teh_final_csharp_project/QueueManager.cs
@@ -4,10 +4,22 @@ public class QueueManager
 {
     private readonly List<int> _playerQueue = new List<int>();
 
-    public void EnqueuePlayer(int playerId)
+    /// <summary>
+    /// Retourne valeur True si le joueur a déjà était ajouté.
+    /// Retourne valeur False si le joueur est déjà dans la liste
+    /// </summary>
+    /// <param name="playerId">Int userid in db</param>
+    /// <returns></returns>
+    public bool EnqueuePlayer(int playerId)
     {
+        if (_playerQueue.Contains(playerId))
+        {
+            return false;
+        }
         _playerQueue.Add(playerId);
+        return true;
     }
+
     
     public List<int> GetQueuedPlayerIds() => _playerQueue.ToList();
 
